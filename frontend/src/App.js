9f7bc3ae3f696eee818c7a2f9a9c3489fa8adee8
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2020-07-26 13:42:55 +0200

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (!data) {
+      fetch('http://localhost:4000/')
+        .then(res => res.json())
+        .then(data => setData(data));
+    }
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {data}
+      </header>
+    </div>
+  );
+}
+
+export default App;
